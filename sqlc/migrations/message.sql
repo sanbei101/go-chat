e99af856325477e393f1d5eebb7dbd454df997f2
sanbei101 <sanbei101@outlook.com>
2026-04-29 14:22:00 +0800

    完善类型

--- sqlc/migrations/message.sql
+++ sqlc/migrations/message.sql
@@ -43,14 +43,13 @@ CREATE TABLE messages (
     client_msg_id uuid NOT NULL,
     sender_id uuid NOT NULL,
     room_id uuid NOT NULL,
-    chat_type chat_type NOT NULL,
-    server_time BIGINT NOT NULL,
     reply_to_msg_id uuid DEFAULT NULL,
     msg_type message_type NOT NULL,
+    server_time BIGINT NOT NULL,
     payload JSONB NOT NULL,
-    ext JSONB DEFAULT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    ext JSONB DEFAULT NULL
 );
+
 -- 用于查询"某个 Room 的所有成员"
 CREATE INDEX idx_room_members_user_id ON room_members (user_id);
 -- 客户端进入某个 Room 时,按时间倒序拉取历史消息
